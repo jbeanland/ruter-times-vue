9544c00d893f126e91230be95944c551ee493152
Jack Beanland <jmcbeanland@gmail.com>
2018-08-30 01:18:27 +0200

    dist updated.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+// TODO:
+// webpack performance recommendations:
+// You can limit the size of your bundles by using import() or require.ensure to lazy load some parts of your application.
+// For more info visit https://webpack.js.org/guides/code-splitting/
+
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
