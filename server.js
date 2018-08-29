3e4911eb3ed7b0d63501ced969b37b80de60c331
Jack Beanland <jmcbeanland@gmail.com>
2018-08-29 12:00:08 +0200

    server.js correction.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
 })
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log('Listening on port ' + port);
 })
